f97b304af1a065fe1f2a19517a3e546e953ac627
bens4lsu <bens4lsu@gmail.com>
2026-09-01 21:45:10 +0000

    contact form work

--- pages/Contacts/jsobjects/contacts/contacts.js
+++ pages/Contacts/jsobjects/contacts/contacts.js
@@ -1,13 +1,60 @@
 export default {
-  async tableInit() {
-    await authorization.refreshTokenIfNeeded();
-    
-		var types = await qryContactTypes.run();
-		types = types.sort((a, b) => a.name.localeCompare(b.name));
-		const contactTypes = types.map(e => ({name: e.name, code: e.id}));
+	async tableInit() {
+		await authorization.refreshTokenIfNeeded();
+
+		let types = await qryContactTypes.run();
+		types = (types || []).sort((a, b) => a.name.localeCompare(b.name));
+
+		const contactTypes = [{ name: "", value: "" }, ...types.map(e => ({ name: e.name, value: e.id }))];
 		await storeValue("contactTypes", contactTypes);
-		
-    const contacts = await qryContacts.run();
-    return contacts;
-  },
+
+		const contacts = await qryContacts.run();
+		return contacts;
+	},
+
+	contactTypeObject: () => {
+		const list = appsmith.store.contactTypes || [];
+		const mapped = list.map(e => ({ name: e.name, value: e.id }));
+
+		return [{ name: "", value: "" }, ...mapped];
+	},
+
+	handleEditClick: async (rowData) => {
+		await storeValue('formDefaults', {
+			id: crypto.randomUUID(), 
+			name: "",
+			address: "",
+			city: "",
+			state: "",
+			zip: "",
+			office_phone: "",
+			mobile_phone: "",
+			email: "",
+			latitude: "",
+			longitude: "",
+			vhf: "",
+			notes: ""
+		}); 
+		showModal(modalContactEntry.name);
+	},
+
+	handleNewClick: async (rowData) => {
+		await storeValue('formDefaults', {
+			id: rowData.id || "ERROR", 
+			name: rowData.name || "",
+			type: rowData.contact_type || ""
+			address: rowData.address || "",
+			city: rowData.city || "",
+			state: rowData.state || "",
+			zip: rowData.zip || "",
+			office_phone: rowData.office_phone || "",
+			mobile_phone: rowData.mobile_phone || "",
+			email: rowData.email || "",
+			latitude: rowData.latitude || "",
+			longitude: rowData.longitude || "",
+			vhf: rowData.vhf || "",
+			notes: rowData.notes || ""
+		}); 
+		showModal(modalContactEntry.name);
+	},
 }
